925581e807682be8a7b9009d73c3eb999d4f25ff
Dreamer223 <drackfil@gmail.com>
2023-08-21 21:18:06 +0300

    Сделал проверки до конца и конструктор. Надо как то узнавать на каком этаже у нас лифт

--- src/main/java/lesson6/lesson.java
+++ src/main/java/lesson6/lesson.java
@@ -4,7 +4,7 @@ public class lesson {
     public static void main(String[] args) {
         Elevator residentialBuilding = new Elevator(15, 2);
         residentialBuilding.move(2);
-        residentialBuilding.getThisFloor();
+
 
     }
 }
