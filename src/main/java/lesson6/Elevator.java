7de36ed580721de4b65b5e381a4a5e4e73123e79
Dreamer223 <drackfil@gmail.com>
2023-08-23 20:54:40 +0300

    Готов

--- src/main/java/lesson6/Elevator.java
+++ src/main/java/lesson6/Elevator.java
@@ -4,12 +4,16 @@ public class Elevator{
         private int maxFloor;
         private  int minFloor=-10;
         private int thisFloor = minFloor;
+        private String range;
+
+
     public Elevator(int maxFloor, int minFloor) {
         if(minFloor > maxFloor){
             throw new IllegalArgumentException("Введите верный диапозон!!!");
         }
         this.maxFloor = maxFloor;
         this.minFloor = minFloor;
+        this.range = "["+minFloor+"-"+maxFloor+"]";
     }
     public Elevator(int maxFloor){
         this(maxFloor, 1);
@@ -25,4 +29,9 @@ public class Elevator{
                 System.out.println("Введите верный этаж!\n Диапозон ["+minFloor+"-"+maxFloor+"]");
              }
     }
+    public String toString(String s) {
+        s=range + " Этаж "+ thisFloor;
+        return s;
+    }
+
 }
--- src/main/java/lesson6/lesson.java
+++ src/main/java/lesson6/lesson.java
@@ -2,8 +2,11 @@ package lesson6;
 
 public class lesson {
     public static void main(String[] args) {
-        Elevator residentialBuilding = new Elevator(15, 2);
-        residentialBuilding.move(2);
+        String s="Floor";
+        Elevator residentialBuilding = new Elevator(17, -3);
+        residentialBuilding.move(17);
+        System.out.println(residentialBuilding.toString(s));
+
 
 
     }
